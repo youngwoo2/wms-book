da8705e59bc501da8665701951dab2c7b4efd6aa
nnieun <abocado216@naver.com>
2024-05-29 21:36:58 +0900

    Update: DB구축 sql 추가 #7

---
+++ KimNakyoung/wmsbook/sql/first.sql
@@ -0,0 +1,6 @@
+# 로컬 mysql에 계정(root) book/book
+create user 'book'@'%' identified by 'book';
+# 스키마 wms_book_db를 생성
+create database wms_book_db;
+# 만든 계정에 만든스키마 모든 권한 주기
+grant all privileges on wms_book_db.* to 'book'@'%'; 
