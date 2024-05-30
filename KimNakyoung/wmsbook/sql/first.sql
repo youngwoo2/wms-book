7bb555af6e27adaf2374fd877cb0efc3823a8541
nnieun <abocado216@naver.com>
2024-05-30 10:56:49 +0900

    Feat : 환경세팅 및 테스트 코드 #7

--- KimNakyoung/wmsbook/sql/first.sql
+++ KimNakyoung/wmsbook/sql/first.sql
@@ -3,4 +3,4 @@ create user 'book'@'%' identified by 'book';
 # 스키마 wms_book_db를 생성
 create database wms_book_db;
 # 만든 계정에 만든스키마 모든 권한 주기
-grant all privileges on wms_book_db.* to 'book'@'%'; 
+grant all privileges on wms_book_db.* to 'book'@'%';
