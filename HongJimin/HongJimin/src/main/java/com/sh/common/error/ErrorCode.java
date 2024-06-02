905095f9387fb930e005cbf5a9d3a605df1fb65f
Hongjimin <jmh907@naver.com>
2024-06-03 01:28:02 +0900

    Feat : ErrorView/ErrorCode 추가 #58

--- HongJimin/HongJimin/src/main/java/com/sh/common/error/ErrorCode.java
+++ HongJimin/HongJimin/src/main/java/com/sh/common/error/ErrorCode.java
@@ -1,2 +1,15 @@
-package com.sh.common;public enum ErrorCode {
+package com.sh.common.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+
+
+public enum ErrorCode {
+    FIND_All_BOOK_ERROR("도서 전체 조회 중 오류가 발생했습니다."),
+    FIND_A_BOOK_ERROR("한 권의 도서 조회 중 오류가 발생했습니다.");
+
+    @Getter
+    final String msg;
 }
