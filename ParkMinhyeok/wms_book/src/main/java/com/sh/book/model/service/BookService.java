f1af520e8176070825fad68a5f1552e815d39469
ParkMinhyeok <parkminem@gmail.com>
2024-06-03 00:56:42 +0900

    Fix: code refactoring in BookService class

--- ParkMinhyeok/wms_book/src/main/java/com/sh/book/model/service/BookService.java
+++ ParkMinhyeok/wms_book/src/main/java/com/sh/book/model/service/BookService.java
@@ -9,35 +9,28 @@ import java.util.List;
 import static com.sh.common.MyBatisTemplate.getSqlSession;
 
 public class BookService {
+    SqlSession sqlSession = getSqlSession();
+    BookMapper bookMapper = sqlSession.getMapper(BookMapper.class);
 
     public List<BookDto> findAll() {
-        // Todo
-        SqlSession sqlSession = getSqlSession();
-        BookMapper bookMapper = sqlSession.getMapper(BookMapper.class);
         List<BookDto> bookList = bookMapper.findAll();
         sqlSession.close();
         return bookList;
     }
 
     public BookDto findByBookId(int bookId) {
-        SqlSession sqlSession = getSqlSession();
-        BookMapper bookMapper = sqlSession.getMapper(BookMapper.class);
         BookDto book = bookMapper.findByBookId(bookId);
         sqlSession.close();
         return book;
     }
 
     public BookDto findByBookTitle(String title) {
-        SqlSession sqlSession = getSqlSession();
-        BookMapper bookMapper = sqlSession.getMapper(BookMapper.class);
         BookDto book = bookMapper.findByBookTitle(title);
         sqlSession.close();
         return book;
     }
 
     public BookDto findByAuthor(String author) {
-        SqlSession sqlSession = getSqlSession();
-        BookMapper bookMapper = sqlSession.getMapper(BookMapper.class);
         BookDto book = bookMapper.findByBookAuthor(author);
         return book;
     }
