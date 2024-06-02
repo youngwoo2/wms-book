f01541315959fb8a8252f634c1eb2ae5eb6dff02
Hongjimin <jmh907@naver.com>
2024-06-03 01:22:54 +0900

    Feat : UserLoginCheck TestCode 작성 #57

--- HongJimin/HongJimin/src/test/java/com/sh/user/model/dao/UserMapperTest.java
+++ HongJimin/HongJimin/src/test/java/com/sh/user/model/dao/UserMapperTest.java
@@ -1,2 +1,41 @@
-package com.sh.user.model.dao;public class UserMapperTest {
+package com.sh.user.model.dao;
+
+import com.sh.user.model.entity.User;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.*;
+
+import static com.sh.common.MyBatisTemplate.getSqlSession;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class UserMapperTest {
+    SqlSession sqlSession;
+    UserMapper userMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.sqlSession = getSqlSession();
+        this.userMapper = this.sqlSession.getMapper(UserMapper.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.sqlSession.close();
+    }
+
+    @DisplayName("로그인 체크")
+    @Test
+    void loginCheck() {
+        // given
+        String username = "도서관리자1";
+        String password = "password123";
+
+        // when
+        User user = userMapper.loginCheck(username, password);
+
+        // then
+        assertThat(user).isNotNull();
+        assertThat(user.getUserName()).isEqualTo(username);
+        assertThat(user.getPassword()).isEqualTo(password);
+    }
 }
