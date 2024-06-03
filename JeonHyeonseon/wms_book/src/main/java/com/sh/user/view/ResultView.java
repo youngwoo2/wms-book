4227d5f215917d31460a53aca0efadc9eb8a1c10
jeonhyeonseon <good_pretty@naver.com>
2024-06-03 17:50:45 +0900

    Update: [3일] 사용자가 로그인을 했는지 확인하는 과정 추가 #40

--- JeonHyeonseon/wms_book/src/main/java/com/sh/user/view/ResultView.java
+++ JeonHyeonseon/wms_book/src/main/java/com/sh/user/view/ResultView.java
@@ -8,7 +8,8 @@ public class ResultView {
     public static void displayLoginResult(UserDto userDto) {
         if (userDto != null) {
             UserDto loginUser = AuthManager.getLoginUser();
-            System.out.printf("안녕하세요. %s님", loginUser.getUserName());
+            System.out.printf("안녕하세요. %s님\n", loginUser.getUserName());
+            System.out.println();
         } else {
             System.out.println("아이디/비밀번호가 일치하지 않습니다.");
         }
