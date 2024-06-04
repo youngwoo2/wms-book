0777725e9da9a5dd2c98d1a2ca82b16abb4ef136
ParkMinhyeok <parkminem@gmail.com>
2024-06-05 01:20:44 +0900

    Feat: 주문 등록 테스트 완료, view 진행중 #87

---
+++ ParkMinhyeok/wms_book/src/test/java/com/sh/order/model/dao/OrderMapperTest.java
@@ -0,0 +1,79 @@
+package com.sh.order.model.dao;
+
+import com.sh.order.model.dto.OrderDto;
+import com.sh.order.model.entity.Status;
+import com.sh.order_item.model.dto.OrderItemDto;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static com.sh.common.MyBatisTemplate.getSqlSession;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderMapperTest {
+    SqlSession sqlSession;
+    OrderMapper orderMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.sqlSession = getSqlSession();
+        this.orderMapper = this.sqlSession.getMapper(OrderMapper.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+            this.sqlSession.close();
+    }
+
+    @Test
+    void insertOrder() {
+        // given
+        OrderDto order = new OrderDto();
+        order.setOrdererName("박민혁");
+        order.setOrdererAddress("용인시 기흥구");
+        order.setOrderDate(LocalDateTime.now());
+        order.setStatus(Status.주문확인중);
+
+        // when
+        int result = orderMapper.insertOrder(order);
+        sqlSession.commit();
+
+        // then
+        assertEquals(1, result);
+        assertNotNull(order.getOrderId());
+        System.out.println("Order ID : " + order.getOrderId());
+    }
+
+    @Test
+    void insertOrderItem() {
+        // Given
+        OrderDto order = new OrderDto();
+        order.setOrdererName("박민혁");
+        order.setOrdererAddress("용인시 기흥구");
+        order.setOrderDate(LocalDateTime.now());
+        order.setStatus(Status.주문확인중);
+
+        // 먼저 주문을 삽입하여 order_id를 생성
+        int orderResult = orderMapper.insertOrder(order);
+        sqlSession.commit();
+        assertEquals(1, orderResult);
+        assertNotNull(order.getOrderId());
+
+        OrderItemDto orderItem = new OrderItemDto();
+        orderItem.setOrderId(order.getOrderId());
+        orderItem.setBookId(1);
+        orderItem.setQuantity(2);
+
+        // When
+        int result = orderMapper.insertOrderItem(orderItem);
+        sqlSession.commit();
+
+        // Then
+        assertEquals(1, result);
+        assertNotNull(orderItem.getOrderItemId());
+        System.out.println("Order Item ID: " + orderItem.getOrderItemId());
+    }
+}
