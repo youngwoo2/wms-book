fc137b089e3ead0b35a0c7ddba5140b0ad212080
jeonhyeonseon <good_pretty@naver.com>
2024-06-05 17:16:55 +0900

    Update: 도서 주문View 구현중..#85

--- JeonHyeonseon/wms_book/src/main/java/com/sh/order/model/dto/OrderItemDto.java
+++ JeonHyeonseon/wms_book/src/main/java/com/sh/order/model/dto/OrderItemDto.java
@@ -1,5 +1,6 @@
 package com.sh.order.model.dto;
 
+import com.sh.book.model.dto.BookDto;
 import lombok.*;
 @Data // @ToString, @EqualsAndHashCode, @Getter, @Setter, @RequiredArgsConstructor
 @NoArgsConstructor
@@ -10,5 +11,5 @@ public class OrderItemDto {
     private int bookId; // 도서 아이디
     private int quantity; // 수량
 
-    private OrderDto order;
+    private BookDto bookDto; // 사용자가 주문한 정보를 확인할 때 책 제목을 넣기 위해서 필요함.
 }
