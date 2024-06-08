6f7e59a27a7b9f27dcf4fa228ba7d83d19ad1112
nnieun <abocado216@naver.com>
2024-06-08 17:30:01 +0900

    Feat: orderItemDto 추가 및 기본설정파일추가 #83

--- KimNakyoung/wmsbook/src/main/java/com/sh/order/view/OrderView.java
+++ KimNakyoung/wmsbook/src/main/java/com/sh/order/view/OrderView.java
@@ -2,7 +2,10 @@ package com.sh.order.view;
 // 1. 주문 생성 (고객 주문 생성 (주문 번호, 고객 정보, 주문 도서, 수량, 배송지 정보 등))
 // 2. 주문 처리 ("주문 상태 관리 (주문 접수, 처리 중, 배송 중, 완료 등)")
 
-
+//1. 임의의 고객주문서가 있다고 가정하고, 이를 토대로 도서관리자는 도서관리앱에 주문을 등록합니다.
+//2. 하나의 주문에는 여러종류의 책을 포함할 수 있으므로, tbl_order 테이블과 tbl_order_item테이블의 관계를 고려해야 합니다.
+//3. 주문 등록시에는 한건의 tbl_order 등록 쿼리와 한건이상의 tbl_order_item 등록쿼리를 함께 트랜잭션처리해야 합니다.
+//4. 다음 샘플 도서주문서를 등록할 수 있도록 있도록 합니다.
 import com.sh.order.controller.OrderController;
 
 import java.util.Scanner;
